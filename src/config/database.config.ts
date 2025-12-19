253e9fe61f8852ded30de14969c351603a708124
Dara Marotto <daramarotto4@gmail.com>
2025-12-19 19:23:30 -0300

    fix

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,7 +4,7 @@ export default registerAs('database', () => ({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 
