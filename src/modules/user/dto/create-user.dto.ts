0a0dc3796d1b28eaba1d9710de6f49b2a1dfa32b
Dara Marotto <daramarotto4@gmail.com>
2025-12-19 19:24:16 -0300

    creating userDTO with class-validator and class-transformer

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, Matches } from "class-validator";
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+
+    @IsEmail()
+    //validate if email is unique
+    email: string;
+
+    @IsNotEmpty()
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*\W+).{6,30}$/, {
+    message:
+      'The password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be between 6 and 30 characters long.',
+  })
+    password: string;
+}
