ffe188b714b3a929534e8e726a05061c5a7d92cf
Dara Marotto <daramarotto4@gmail.com>
2026-03-12 11:28:36 -0300

    fix cache tll

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,7 +26,7 @@ import * as redisStore from "cache-manager-redis-store"
             store: redisStore,
             host: process.env.REDIS_HOST,
             port: process.env.REDIS_PORT,
-            ttl: 60,
+            ttl: Number(process.env.REDIS_TTL),
         })
     ],
     providers: [],
